15dc56ff40ffea577129094a49751034700fea0b
Corrie Kay <corriekay.pinkie@gmail.com>
2012-04-12 19:05:02 -0700

    added string > inventory AND inventory > string Functions
    Signed-off-by: Corrie Kay <corriekay.pinkie@gmail.com>

--- src/com/github/CorrieKay/PinkiePiesPlethoraOfPoniPlugins/Handlers/InventorySee.java
+++ src/com/github/CorrieKay/PinkiePiesPlethoraOfPoniPlugins/Handlers/InventorySee.java
@@ -2,14 +2,18 @@ package com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.Handlers;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
 import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.Mane;
 import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.utils.LookitInventory;
@@ -63,5 +67,51 @@ public class InventorySee extends PoniCommandExecutor implements Listener{
 		if(viewingInventoryLive.containsKey(player)){
 			Bukkit.getScheduler().scheduleAsyncDelayedTask(instance, new LookitInventory(viewingInventoryLive.get(player),player), 1);
 		}
+	}
+	/**
+	 * @Functions here deal with inventory translation, from stringList to inventory, and from inventory to StringList
+	 */
+	public static ItemStack[] toInventory(List<String> inventoryString){
+		ItemStack[] items = new ItemStack[36];
+		int number = 0;
+		for(String stack : inventoryString){
+			if(!stack.equals("air")){
+				String[] bits = stack.split(" ");
+				Material mat = Material.getMaterial(Integer.parseInt(bits[0]));
+				int ammount = Integer.parseInt(bits[1]);
+				short durability = Short.parseShort(bits[2]);
+				byte data = Byte.parseByte(bits[3]);
+				ItemStack item = new ItemStack(mat,ammount, durability, data);
+				HashMap<Enchantment, Integer> enchantments = new HashMap<Enchantment, Integer>();
+				for(int i = 4 ; i<bits.length;i=i+2){
+					enchantments.put(Enchantment.getById(Integer.parseInt(bits[i])), Integer.parseInt(bits[i+1]));
+				}
+				item.addUnsafeEnchantments(enchantments);
+				items[number] = item;
+			} else {
+				items[number] = new ItemStack(Material.AIR);
+			}
+			number++;
+		}
+		return items;
+	}
+	public static List<String> toStringList(ItemStack[] inventory){
+		List<String> stringInventory = new ArrayList<String>();
+		for(ItemStack stack : inventory){
+			if(stack == null){
+				stringInventory.add("air");
+			} else {
+				String invToString = "";
+				invToString += stack.getTypeId();
+				invToString+=" "+stack.getAmount();
+				invToString+=" "+stack.getDurability();
+				invToString+=" "+stack.getData().getData();
+				for(Enchantment enchant : stack.getEnchantments().keySet()){
+					invToString+=" "+enchant.getId()+" "+stack.getEnchantmentLevel(enchant);
+				}
+				stringInventory.add(invToString);
+			}
+		}
+		return stringInventory;
 	}
 }
