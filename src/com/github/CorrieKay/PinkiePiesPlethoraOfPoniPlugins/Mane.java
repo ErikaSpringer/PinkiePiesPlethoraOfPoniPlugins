8c241328fb363dcd320c323a757065969fd2740d
Corrie Kay <corriekay.pinkie@gmail.com>
2012-04-13 16:46:12 -0700

    managed imports, added a permdeny message to all commands
    Signed-off-by: Corrie Kay <corriekay.pinkie@gmail.com>

--- src/com/github/CorrieKay/PinkiePiesPlethoraOfPoniPlugins/Mane.java
+++ src/com/github/CorrieKay/PinkiePiesPlethoraOfPoniPlugins/Mane.java
@@ -1,6 +1,8 @@
 package com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,8 +11,14 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.Handlers.*;
-import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.utils.*;
+import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.Handlers.AFKhandler;
+import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.Handlers.ConfigHandler;
+import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.Handlers.InventorySee;
+import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.Handlers.InvisibilityHandler;
+import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.Handlers.JoinHandler;
+import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.Handlers.QuitHandler;
+import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.utils.PSJoinEvent;
+import com.github.CorrieKay.PinkiePiesPlethoraOfPoniPlugins.utils.PSQuitEvent;
 
 public class Mane extends JavaPlugin implements Listener{
 	private final AFKhandler afk = new AFKhandler(this);
@@ -22,6 +30,9 @@ public class Mane extends JavaPlugin implements Listener{
 	
 	public void onEnable(){
 		PluginManager pm = Bukkit.getPluginManager();
+		for(String string : YamlConfiguration.loadConfiguration(this.getResource("plugin.yml")).getConfigurationSection("commands").getKeys(false)){
+			this.getCommand(string).setPermissionMessage(ChatColor.LIGHT_PURPLE+"[ServerGuardian] Pinkie Pie: You cant do this :c");
+		}
 		afk.initialize();
 		ih.initialize();
 		is.initialize();
